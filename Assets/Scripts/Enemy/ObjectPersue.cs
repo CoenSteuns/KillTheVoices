34362b79987844d25f2bde118b7aa2049b77e0fd
jvdwijk <unconfigured@null.spigotmc.org>
2018-01-21 18:58:36 +0100

    Enemy distance

--- Assets/Scripts/Enemy/ObjectPersue.cs
+++ Assets/Scripts/Enemy/ObjectPersue.cs
@@ -4,6 +4,7 @@ public class ObjectPersue : MonoBehaviour
 {
 	[SerializeField] private float _maxSpeed = 3;
 	[SerializeField] private Transform _target;
+	[SerializeField] private float _keptDistance;
     private Vector2 _currentSpeed;
     private Vector2 _objectPosition;
    
@@ -20,13 +21,16 @@ public class ObjectPersue : MonoBehaviour
 
 	private void Persue() 
 	{    
-        var direction = (Vector2)_target.position - _objectPosition;
-        direction.Normalize();
+        var distance = (Vector2)_target.position - _objectPosition;
+		if(distance.magnitude < _keptDistance) return;
+		
+        var direction = distance.normalized;
+		//print(direction);
         var wantedSpeed = direction * _maxSpeed;
  
         var movementForce = wantedSpeed - _currentSpeed;
         _currentSpeed += movementForce;
-
+		
         _objectPosition += _currentSpeed * Time.deltaTime;
         transform.position = _objectPosition;
 		
